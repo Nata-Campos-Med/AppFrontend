0d4a7f9647f5ac6dd68bd4272e5992fc61293e3a
Cristian Castañeda <crisleoco14@gmail.com>
2021-10-02 19:45:03 -0500

    Bugs reparados

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -5,14 +5,6 @@ THEME_SWITCH.addEventListener("click", () => {
     BODY.classList.toggle("light-theme")
 })
 
-// const BTN_HOME = document.getElementById("btn-home")
-// const BTN_USER = document.getElementById("btn-users")
-// const BTN_CUSTOMER = document.getElementById("btn-customers")
-// const BTN_SUPPLIER = document.getElementById("btn-suppliers")
-// const BTN_PRODUCT = document.getElementById("btn-products")
-// const BTN_SALE = document.getElementById("btn-sales")
-// const BTN_REPORT = document.getElementById("btn-reports")
-
 const WELCOME_MSG = document.getElementById("welcome-message")
 
 //Página de bienvenida
@@ -21,7 +13,7 @@ const RANDOM_NUM = (min, max) => {
     let num = Math.random()*(max-min) + min
     return Math.floor(num)
 }
-WELCOME_IMG.setAttribute('src', `../images/${RANDOM_NUM(1, 8)}.png`)
+WELCOME_IMG.setAttribute('src', `./images/${RANDOM_NUM(1, 8)}.png`)
 
 ///CERRAR MENSAJES con Boton
 let btnCloseMessage = document.getElementsByClassName('close-message')
@@ -31,25 +23,3 @@ for(let i = 0; i < messages.length; i++) {
         messages[i].style.display = 'none'
     })
 }
-
-// BTN_HOME.addEventListener('click', () => {
-//     window.location.href = '../home.html'
-// })
-// BTN_USER.addEventListener('click', () => {
-//     window.location.href = '../users.html'
-// })
-// BTN_CUSTOMER.addEventListener('click', () => {
-//     window.location.href = '../customers.html'
-// })
-// BTN_SUPPLIER.addEventListener('click', () => {
-//     window.location.href = '../suppliers.html'
-// })
-// BTN_PRODUCT.addEventListener('click', () => {
-//     window.location.href = '../products.html'
-// })
-// BTN_SALE.addEventListener('click', () => {
-//     window.location.href = '../sales.html'
-// })
-// BTN_REPORT.addEventListener('click', () => {
-//     window.location.href = '../reports.html'
-// })
--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,24 +1,26 @@
+//Ver u ocultar contraseña
 let btnViewOrHidePassword = document.getElementById('btn-view-password')
 let inputPassword = document.getElementById('input-password')
-let inputUser = document.getElementById('input-user')
-let errorContainer = document.getElementById('form-message-error')
-let btnSubmit = document.getElementById('btn-submit')
-let btnCloseMessage = document.getElementById('close-message')
-
-
-//Ver u ocultar contraseña
 btnViewOrHidePassword.addEventListener('click', () => {
     if(inputPassword.attributes[2].value === 'password') {
         inputPassword.setAttribute('type', 'text')
-        btnViewOrHidePassword.style.backgroundImage = 'url("../images/close-eye.png")'
+        btnViewOrHidePassword.style.backgroundImage = 'url("./images/close-eye.png")'
     } else if(inputPassword.attributes[2].value === 'text') {
         inputPassword.setAttribute('type', 'password')
-        btnViewOrHidePassword.style.backgroundImage = 'url("../images/eye.png")'
+        btnViewOrHidePassword.style.backgroundImage = 'url("./images/eye.png")'
     }
 })
 
-//ALTERNAR MODO OSCURO
+//Cerrar mensaje
+let errorContainer = document.getElementById('form-message-error')
+let btnCloseMessage = document.getElementById('close-message')
+btnCloseMessage.addEventListener('click', () => {
+    errorContainer.style.display = 'none'
+})
 
+
+
+//ALTERNAR MODO OSCURO
 let body = document.body
 let themeSwitch = document.getElementById("theme-switch")
 
